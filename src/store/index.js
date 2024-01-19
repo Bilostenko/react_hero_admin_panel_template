22cc1356c654a80d0f181b9e2a4cfdb4cb7af7b7
Bilostenko <bilostenkod@gmail.com>
2024-01-19 23:16:27 +0200

    add ReduxThunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,29 +1,24 @@
-import { legacy_createStore as createStore, compose } from 'redux';
+import { legacy_createStore as createStore, compose, applyMiddleware } from 'redux';
 import { combineReducers } from 'redux';
+import ReduxThunk from 'redux-thunk';
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
 
-const enhancer = (createStore) => (...args) => {
-    const store = createStore(...args);
-
-    const oldDispatch = store.dispatch;
-    store.dispatch = (action) => {
-        if (typeof action === 'string') {
-            return oldDispatch({
-                type: action
-            })
-        }
-        return oldDispatch(action)
+const stringMiddleware = () => (next) => (action) => {
+    if (typeof action === 'string') {
+        return next({
+            type: action
+        })
     }
-    return store;
-}
+    return next(action)
+};
 
 const store = createStore( 
-    combineReducers({heroes, filters}),
-    compose(
-        enhancer,
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    ))
+                combineReducers({heroes, filters}),
+                compose(applyMiddleware(ReduxThunk, stringMiddleware),
+                        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+                );
+
 
 export default store;
 
