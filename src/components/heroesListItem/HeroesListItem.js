ce41a22e5fba1262f9b0b28232e65764fc18639a
Bilostenko <bilostenkod@gmail.com>
2024-04-18 00:05:23 +0300

    change face icon

--- src/components/heroesListItem/HeroesListItem.js
+++ src/components/heroesListItem/HeroesListItem.js
@@ -23,7 +23,7 @@ const HeroesListItem = ({name, description, element, onDelete}) => {
     return (
         <li 
             className={`card flex-row mb-4 shadow-lg text-white ${elementClassName}`}>
-            <img src="http://www.stpaulsteinbach.org/wp-content/uploads/2014/09/unknown-hero.jpg" 
+            <img src="https://staticdelivery.nexusmods.com/mods/1151/images/thumbnails/10080-0-1455866940.png" 
                  className="img-fluid w-25 d-inline" 
                  alt="unknown hero" 
                  style={{'objectFit': 'cover'}}/>
